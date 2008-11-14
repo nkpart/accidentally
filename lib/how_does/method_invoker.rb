6737d4a7b7882b2f293db6eed672b9cfdbce79de
Nick Partridge <nkpart@gmail.com>
2008-11-14 20:14:19 +1030

    Threaded execution of methods in case they are limitless. 5 second timeout.

--- lib/how_does/method_invoker.rb
+++ lib/how_does/method_invoker.rb
@@ -11,7 +11,7 @@ module HowDoes
     def invoke meth
       v, warns = yield_and_collect_warns {
         begin
-          x = @v.dup.send(meth)
+          x = do_send(meth)
           if defined?(Enumerable::Enumerator) && x.is_a?(Enumerable::Enumerator) then
             invoke_block_method(meth)
           else 
@@ -25,9 +25,19 @@ module HowDoes
       v
     end
     
+    def do_send meth, &blk
+      # Executing with timeout in case of endless methods, like Array#cycle in 1.8.7
+      r = nil
+      t = Thread.new {
+        r = @v.dup.send(meth, &blk)
+      }
+      t.join(5)
+      r
+    end
+    
     def invoke_block_method m
       blk = proc { |x| x }
-      v= @v.dup.send(m, &blk)
+      v= do_send(m, &blk)
     end
     
     def yield_and_collect_warns
