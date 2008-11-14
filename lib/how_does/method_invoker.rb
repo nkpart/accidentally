e925c2f7b099f5472d76ba491e3bcaf11fa885b9
Nick Partridge <nkpart@gmail.com>
2008-11-14 19:33:45 +1030

    Pulling warnings out now

--- lib/how_does/method_invoker.rb
+++ lib/how_does/method_invoker.rb
@@ -11,23 +11,20 @@ module HowDoes
     def invoke meth
       v = nil
       warns = collect_warns {
-        v = @v.send(meth)
+        begin
+          v = @v.send(meth)          
+        rescue LocalJumpError => e
+          blk = proc { |x| x }
+          v= @v.send(meth, &blk)   
+        end
       }
       p warns if !warns.empty?
       v
-    rescue LocalJumpError => e
-      blk = proc { |x| x }
-      @v.send(meth, &blk)
     end
     
     def collect_warns
       old_err = $stderr
       new_err = StringIO.new
-      class <<new_err
-        def write v
-          super v
-        end
-      end
       $stderr = new_err
       yield
       $stderr = old_err
