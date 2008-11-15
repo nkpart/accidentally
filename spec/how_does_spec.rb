36ddd0a669ed6184deead90ac0ee978e088adaf0
Paul O'Keeffe <pgokeeffe@gmail.com>
2008-11-15 16:40:19 +1000

    Include HowDoes into spec to demonstrate more succinct calls.

--- spec/how_does_spec.rb
+++ spec/how_does_spec.rb
@@ -1,52 +1,54 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-describe "HowDoes" do
+include HowDoes
+
+describe HowDoes do
   it "should figure out length of an array" do
-    methods = HowDoes.how_does("dog").become(3)
+    methods = how_does("dog").become(3)
     assert methods.include?("length")
   end
   
   it "should figure out array reversal" do
-    methods = HowDoes.how_does([1,2]).become([2,1])
+    methods = how_does([1, 2]).become([2, 1])
     assert methods.include?("reverse")
   end
   
   it "should figure out select" do
-    methods = HowDoes.how_does([1,false,2]).become([1,2])
+    methods = how_does([1, false, 2]).become([1, 2])
     assert methods.include?("select")
   end
   
   it "should figure out index" do
-    methods = HowDoes.how_does([:a, :b, :c]).become(:a)
+    methods = how_does([:a, :b, :c]).become(:a)
     assert methods.include?("first")
   end
   
   it "should be able to be guided by a parameter" do
-    methods = HowDoes.how_does([:a, :b, :c]).with(:b).become(1)
+    methods = how_does([:a, :b, :c]).with(:b).become(1)
     assert methods.include?("index")
   end
 end
 
 describe "Howdy Doodie using cool hash syntax" do
   it "should figure out length of an array when there are no method arguments" do
-    methods = HowDoes.howdy_doodie "dog" => 3
+    methods = howdy_doodie "dog" => 3
     assert methods.include?("length")
   end
   
   it "should work for hashes too" do
-    methods = HowDoes.howdy_doodie({"dog" => 4} => 1)
+    methods = howdy_doodie({"dog" => 4} => 1)
     assert methods.include?("length")
   end  
 end
 
 describe HowDoes::MethodInvoker do
   it "should invoke a simple no args method" do
-    r = HowDoes::MethodInvoker.invoke [], :size, []
+    r = MethodInvoker.invoke [], :size, []
     assert_equal 0, r
   end
   
   it "should invoke a no args method that takes a block" do
-    r = HowDoes::MethodInvoker.invoke [1], :select, []
+    r = MethodInvoker.invoke [1], :select, []
     assert_equal [1], r    
   end
 end
@@ -78,7 +80,6 @@ describe "Kernel patches" do
     warns.should == "fail\n2\n"
   end
 end
-
 
 describe "Thread patches" do
   it "should execute with a timeout" do
